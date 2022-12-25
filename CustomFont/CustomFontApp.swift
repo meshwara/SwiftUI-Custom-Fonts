23868c47abd67c7f12d92e00724829dfe701cfe4
D. Prameswara <meshwara@gmail.com>
2022-12-26 06:55:09 +0700

    Initial Commit

---
+++ CustomFont/CustomFontApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontApp.swift
+//  CustomFont
+//
+//  Created by D. Prameswara on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
